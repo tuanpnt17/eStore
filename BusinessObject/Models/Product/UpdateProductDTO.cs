741d3ddc05ee0673e27f0719b6bd4fa020b4e3ed
fishmakeweb <duongviethoang.1909@gmail.com>
2025-04-02 10:51:12 +0700

    add error message

--- BusinessObject/Models/Product/UpdateProductDTO.cs
+++ BusinessObject/Models/Product/UpdateProductDTO.cs
@@ -6,9 +6,9 @@ public class UpdateProductDTO
 {
 	public int ProductId { get; set; }
 	public int? CategoryId { get; set; }
-	[StringLength(40, ErrorMessage = "Độ dài tên sản phẩm không quá 40 kí tự.")]
-	public string? ProductName { get; set; }
-	[StringLength(20, ErrorMessage = "Độ dài mô tả cân nặng không quá 20 kí tự.")]
+	[Required(ErrorMessage = "Tên sản phẩm là bắt buộc."), StringLength(40, ErrorMessage = "Độ dài tên sản phẩm không quá 40 kí tự.")]
+	public required string ProductName { get; set; }
+	[Required(ErrorMessage = "Cân nặng sản phẩm là bắt buộc."), StringLength(20, ErrorMessage = "Độ dài mô tả cân nặng không quá 20 kí tự.")]
 	public string? Weight { get; set; }
 	[Range(1000, double.MaxValue, ErrorMessage = "Giá không thể thấp hơn 1000.")]
 	public decimal? UnitPrice { get; set; }
